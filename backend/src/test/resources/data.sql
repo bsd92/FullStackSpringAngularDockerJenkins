3b607b184b2dca7fd1fbb4e345f59d25316d7db0
bsd92 <boubacarhb92@gmail.com>
2025-06-23 10:13:25 +0200

    resolve by adding other files properties 17

--- backend/src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO app_user(username, email) VALUES ('johndoe', 'john@example.com');
-
-INSERT INTO car (immatriculation, marque, modele, etat) VALUES ('DZ-568-MJ', 'toyota', 'yaris', 'neuve');
-INSERT INTO car (immatriculation, marque, modele, etat) VALUES ('AB-123-CD', 'peugeot', '308', 'occasion');
-INSERT INTO car (immatriculation, marque, modele, etat) VALUES ('XY-456-ZT', 'renault', 'clio', 'neuve');
